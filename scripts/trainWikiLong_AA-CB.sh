031371b489e5f7e1adde14f67f38e16adae52934
niebuhrc <c.niebuhr@gmail.com>
2017-05-02 10:27:01 +0200

    bugfix

--- scripts/trainWikiLong_AA-CB.sh
+++ scripts/trainWikiLong_AA-CB.sh
@@ -7,17 +7,17 @@ sl=300
 save="../allSaves/$rnns-$numl-$sl-wiki"
 data="../data/bigWikiAllChars"
 
-#init="None"
+#init=None
 init=$save
 
-mkdirs $save
+mkdir $save
 for letterOne in {A..B} ; do
   for letterTwo in {A..Z} ; do
     echo $letterOne$letterTwo
-    python3 trainWiki.py --data_dir=$data/$letterOne$letterTwo/ --save_dir=$save --init_from $init --num_layers numl --rnn_size rnns --num_epochs 1 --batch_size $bs --seq_length $sl --learning_rate $lr
+    python3 trainWiki.py --data_dir=$data/$letterOne$letterTwo/ --save_dir=$save --init_from $init --num_layers $numl --rnn_size $rnns --num_epochs 1 --batch_size $bs --seq_length $sl --learning_rate $lr
   done
 done
 echo CA
-python3 trainWiki.py --data_dir=$data/CA/ --save_dir=$save --init_from $init --num_layers numl --rnn_size rnns --num_epochs 1 --batch_size $bs --seq_length $sl --learning_rate $lr
+python3 trainWiki.py --data_dir=$data/CA/ --save_dir=$save --init_from $init --num_layers $numl --rnn_size $rnns --num_epochs 1 --batch_size $bs --seq_length $sl --learning_rate $lr
 echo CB
-python3 trainWiki.py --data_dir=$data/CB/ --save_dir=$save --init_from $init --num_layers numl --rnn_size rnns --num_epochs 1 --batch_size $bs --seq_length $sl --learning_rate $lr
+python3 trainWiki.py --data_dir=$data/CB/ --save_dir=$save --init_from $init --num_layers $numl --rnn_size $rnns --num_epochs 1 --batch_size $bs --seq_length $sl --learning_rate $lr
