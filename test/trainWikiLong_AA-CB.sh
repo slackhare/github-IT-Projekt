963430a6344a63bde999e0319f1ff8f4978d0e5c
niebuhrc <c.niebuhr@gmail.com>
2017-05-03 12:24:15 +0200

    lade alten status

--- test/trainWikiLong_AA-CB.sh
+++ test/trainWikiLong_AA-CB.sh
@@ -9,4 +9,13 @@ data="../data/bigWikiAllChars"
 init=$save
 
 mkdir $save
-python3 trainWiki.py --data_dir=$data/AA/ --save_dir=$save --init_from $init --num_layers $numl --rnn_size $rnns --num_epochs 1 --batch_size $bs --seq_length $sl --learning_rate $lr
+for letterOne in {A..B} ; do
+  for letterTwo in {A..Z} ; do
+    echo $letterOne$letterTwo
+    python3 trainWiki.py --data_dir=$data/$letterOne$letterTwo/ --save_dir=$save --init_from $init --num_layers $num --num_epochs 1 --batch_size $bs --seq_length $sl --learning_rate $lr
+  done
+done
+echo CA
+python3 trainWiki.py --data_dir=$data/CA/ --save_dir=$save --init_from $init --num_layers $numl --rnn_size $rnns --num_epochs 1 --batch_size $bs --seq_length $sl --learning_rate $lr
+echo CB
+python3 trainWiki.py --data_dir=$data/CB/ --save_dir=$save --init_from $init --num_layers $numl --rnn_size $rnns --num_epochs 1 --batch_size $bs --seq_length $sl --learning_rate $lr
