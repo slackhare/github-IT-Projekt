2827298318bc3a590372f58f9d3785cda3db6b0c
haus <f.haus@ostfalia.de>
2017-05-11 10:48:11 +0200

    maybe on one file...

--- scripts/256-8-300_train.sh
+++ scripts/256-8-300_train.sh
@@ -1,6 +1,6 @@
 rnns=256
 numl=8
-lr=0.004
+lr=0.002
 bs=300
 sl=300
 
@@ -9,13 +9,17 @@ data="../data/bigWikiAllChars"
 init=$save
 
 mkdir $save
-for letterOne in {A..B} ; do
-  for letterTwo in {A..Z} ; do
-    echo $letterOne$letterTwo
-    python3 trainWiki.py --data_dir=$data/$letterOne$letterTwo/ --save_dir=$save --init_from $init --num_layers $numl --rnn_size $rnns --num_epochs 1 --batch_size $bs --seq_length $sl --learning_rate $lr
-  done
-done
-echo CA
-python3 trainWiki.py --data_dir=$data/CA/ --save_dir=$save --init_from $init --num_layers $numl --rnn_size $rnns --num_epochs 1 --batch_size $bs --seq_length $sl --learning_rate $lr
-echo CB
-python3 trainWiki.py --data_dir=$data/CB/ --save_dir=$save --init_from $init --num_layers $numl --rnn_size $rnns --num_epochs 1 --batch_size $bs --seq_length $sl --learning_rate $lr
+
+python3 trainWiki.py --data_dir=$data/AA/ --save_dir=$save --init_from $init --num_layers $numl --rnn_size $rnns --num_epochs 20 --batch_size $bs --seq_length $sl --learning_rate $lr --decay_rate 0.95
+
+
+#for letterOne in {A..B} ; do
+#  for letterTwo in {A..Z} ; do
+#    echo $letterOne$letterTwo
+#    python3 trainWiki.py --data_dir=$data/$letterOne$letterTwo/ --save_dir=$save --init_from $init --num_layers $numl --rnn_size $rnns --num_epochs 1 --batch_size $bs --seq_length $sl --learning_rate $lr
+#  done
+#done
+#echo CA
+#python3 trainWiki.py --data_dir=$data/CA/ --save_dir=$save --init_from $init --num_layers $numl --rnn_size $rnns --num_epochs 1 --batch_size $bs --seq_length $sl --learning_rate $lr
+#echo CB
+#python3 trainWiki.py --data_dir=$data/CB/ --save_dir=$save --init_from $init --num_layers $numl --rnn_size $rnns --num_epochs 1 --batch_size $bs --seq_length $sl --learning_rate $lr
