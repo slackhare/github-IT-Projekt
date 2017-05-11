83caba4463c2f249437d574571121b28bcf321c5
haus <f.haus@ostfalia.de>
2017-05-11 10:30:46 +0200

    extractWiki files

---
+++ extractWiki/merge.sh
@@ -0,0 +1,66 @@
+find text/CA -name "*" -exec cat {} + > bigWiki/CA/input.txt
+find text/CB -name "*" -exec cat {} + > bigWiki/CB/input.txt
+
+find text/BA -name "*" -exec cat {} + > bigWiki/BA/input.txt
+find text/BB -name "*" -exec cat {} + > bigWiki/BB/input.txt
+find text/BC -name "*" -exec cat {} + > bigWiki/BC/input.txt
+find text/BD -name "*" -exec cat {} + > bigWiki/BD/input.txt
+find text/BE -name "*" -exec cat {} + > bigWiki/BE/input.txt
+find text/BF -name "*" -exec cat {} + > bigWiki/BF/input.txt
+
+find text/BG -name "*" -exec cat {} + > bigWiki/BG/input.txt
+find text/BH -name "*" -exec cat {} + > bigWiki/BH/input.txt
+find text/BI -name "*" -exec cat {} + > bigWiki/BI/input.txt
+find text/BJ -name "*" -exec cat {} + > bigWiki/BJ/input.txt
+find text/BK -name "*" -exec cat {} + > bigWiki/BK/input.txt
+find text/BL -name "*" -exec cat {} + > bigWiki/BL/input.txt
+
+find text/BM -name "*" -exec cat {} + > bigWiki/BM/input.txt
+find text/BL -name "*" -exec cat {} + > bigWiki/BN/input.txt
+find text/BO -name "*" -exec cat {} + > bigWiki/BO/input.txt
+find text/BP -name "*" -exec cat {} + > bigWiki/BP/input.txt
+find text/BQ -name "*" -exec cat {} + > bigWiki/BQ/input.txt
+find text/BR -name "*" -exec cat {} + > bigWiki/BR/input.txt
+
+find text/BS -name "*" -exec cat {} + > bigWiki/BS/input.txt
+find text/BT -name "*" -exec cat {} + > bigWiki/BT/input.txt
+find text/BU -name "*" -exec cat {} + > bigWiki/BU/input.txt
+find text/BV -name "*" -exec cat {} + > bigWiki/BV/input.txt
+find text/BW -name "*" -exec cat {} + > bigWiki/BW/input.txt
+find text/BX -name "*" -exec cat {} + > bigWiki/BX/input.txt
+
+find text/BY -name "*" -exec cat {} + > bigWiki/BY/input.txt
+find text/BZ -name "*" -exec cat {} + > bigWiki/BZ/input.txt
+
+
+
+find text/AA -name "*" -exec cat {} + > bigWiki/AA/input.txt
+find text/AB -name "*" -exec cat {} + > bigWiki/AB/input.txt
+find text/AC -name "*" -exec cat {} + > bigWiki/AC/input.txt
+find text/AD -name "*" -exec cat {} + > bigWiki/AD/input.txt
+find text/AE -name "*" -exec cat {} + > bigWiki/AE/input.txt
+find text/AF -name "*" -exec cat {} + > bigWiki/AF/input.txt
+
+find text/AG -name "*" -exec cat {} + > bigWiki/AG/input.txt
+find text/AH -name "*" -exec cat {} + > bigWiki/AH/input.txt
+find text/AI -name "*" -exec cat {} + > bigWiki/AI/input.txt
+find text/AJ -name "*" -exec cat {} + > bigWiki/AJ/input.txt
+find text/AK -name "*" -exec cat {} + > bigWiki/AK/input.txt
+find text/AL -name "*" -exec cat {} + > bigWiki/AL/input.txt
+
+find text/AM -name "*" -exec cat {} + > bigWiki/AM/input.txt
+find text/AL -name "*" -exec cat {} + > bigWiki/AN/input.txt
+find text/AO -name "*" -exec cat {} + > bigWiki/AO/input.txt
+find text/AP -name "*" -exec cat {} + > bigWiki/AP/input.txt
+find text/AQ -name "*" -exec cat {} + > bigWiki/AQ/input.txt
+find text/AR -name "*" -exec cat {} + > bigWiki/AR/input.txt
+
+find text/AS -name "*" -exec cat {} + > bigWiki/AS/input.txt
+find text/AT -name "*" -exec cat {} + > bigWiki/AT/input.txt
+find text/AU -name "*" -exec cat {} + > bigWiki/AU/input.txt
+find text/AV -name "*" -exec cat {} + > bigWiki/AV/input.txt
+find text/AW -name "*" -exec cat {} + > bigWiki/AW/input.txt
+find text/AX -name "*" -exec cat {} + > bigWiki/AX/input.txt
+
+find text/AY -name "*" -exec cat {} + > bigWiki/AY/input.txt
+find text/AZ -name "*" -exec cat {} + > bigWiki/AZ/input.txt
