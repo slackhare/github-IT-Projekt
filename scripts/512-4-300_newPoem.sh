9c1d6be3ec1ecd3b56a86441bdb48e542605aa93
haus <f.haus@ostfalia.de>
2017-06-20 16:39:48 +0200

    fix

--- scripts/512-4-300_newPoem.sh
+++ scripts/512-4-300_newPoem.sh
@@ -7,12 +7,12 @@ sl=300
 
 data="../data/deutscheLyrik"
 init="../allSaves/1-new"
-save = $init
-
+save=$init
+mkdir $save
 python3 trainWiki.py --data_dir=$data/ --save_dir=$save --num_layers $numl --rnn_size $rnns --num_epochs 1 --batch_size $bs --seq_length $sl --learning_rate 0
 
 for myNumber in {1..9} ; do
-  save="../allSaves/$myNumber-retrain"
+  save="../allSaves/$myNumber-new"
   mkdir $save
   python3 trainWiki.py --data_dir=$data/ --save_dir=$save --init_from $init --num_layers $numl --rnn_size $rnns --num_epochs 10 --batch_size $bs --seq_length $sl --learning_rate $lr
   init=$save
