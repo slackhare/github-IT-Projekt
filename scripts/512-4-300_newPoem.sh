3afeea429648a7f0ddf2f9c270fe207dced9bf93
niebuhrc <c.niebuhr@gmail.com>
2017-06-20 16:26:32 +0200

    blal

--- scripts/512-4-300_newPoem.sh
+++ scripts/512-4-300_newPoem.sh
@@ -6,7 +6,10 @@ sl=300
 
 
 data="../data/deutscheLyrik"
-init="../allSaves/$rnns-$numl-$sl-wiki"
+init="../allSaves/1-new"
+save = $init
+
+python3 trainWiki.py --data_dir=$data/ --save_dir=$save --num_layers $numl --rnn_size $rnns --num_epochs 1 --batch_size $bs --seq_length $sl --learning_rate 0
 
 for myNumber in {1..9} ; do
   save="../allSaves/$myNumber-retrain"
